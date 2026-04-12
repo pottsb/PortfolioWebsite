0626169c5e32242b5dc637ad00f6adbdea354a40
PottsB <63508539+pottsb@users.noreply.github.com>
2026-04-12 13:50:28 +0100

    remove unneeded v0 components

--- home-server/portfolio-www/app/layout.tsx
+++ home-server/portfolio-www/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter, JetBrains_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 
